6bfc5dd40dfa6acc11c1274971be0be1a91e6db1
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2024-01-08 08:38:34 +0000

    Attempt to use docker dind and compose with local mounted path at launch.

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,22 @@
-FROM docker:dind
+FROM docker:24.0-dind
 
-RUN apk --no-cache add git bash # docker-compose
+RUN apk --no-cache add bash
 
-ENV OLS_HOME /opt/ols/
+ENV OLS_HOME /app
 RUN mkdir $OLS_HOME
 WORKDIR $OLS_HOME
 
-RUN git clone --branch stable --depth 1 https://github.com/EBISPOT/ols4.git ${OLS_HOME}/ols4
-# ADD ols4 $OLS_HOME
-# ADD configuration files
+ADD ols4 $OLS_HOME/ols4/
+# ADD images $OLS_HOME/
 ADD ols-config.json ${OLS_HOME}/ols4/dataload/configs/
 ADD phi.obo ${OLS_HOME}
 ADD docker-compose.yml ${OLS_HOME}
 ADD entrypoint.sh ${OLS_HOME}
 # set compose CONFIG file path
 ENV OLS4_CONFIG "./dataload/configs/ols-config.json"
+ENV COMPOSE_FILE "${OLS_HOME}/docker-compose.yml"
 
 EXPOSE 8080
-ENTRYPOINT ["/opt/ols/entrypoint.sh"]
+# ENTRYPOINT ["/opt/ols/entrypoint.sh"]
+# "docker", "compose", "up", "-d"]
+CMD ["sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,26 @@
 set -m
 
 # Start docker service in background
-/usr/local/bin/dockerd-entrypoint.sh &
-
+echo "Starting Docker Daemon..."
+/usr/local/bin/dockerd-entrypoint.sh & 2>/dev/null
 while ! docker info; do
   echo "Waiting docker..."
   sleep 3
 done
+echo "Docker Started!"
 
-docker compose -f /opt/ols/docker-compose.yml up --quiet-pull -d
+#echo "Pulling ols4-solr...."
+#docker compose pull --quiet ols4-solr
+#echo "Pulling ols4-neo4j...."
+#docker compose pull --quiet ols4-neo4j
+#echo "Pulling ols4-backend...."
+#docker compose pull --quiet ols4-backend
+#echo "Pulling ols4-dataload...."
+#docker compose pull --quiet ols4-dataload
+#echo "Starting Services!...."
+for file in /opt/ols/images/*.tar; do
+  docker load < $file
+done
 
-echo "Services started"
+docker compose up -d --quiet-pull --no-attach ols4-solr --no-attach ols4-neo4j --no-attach ols4-backend --no-attach ols4-dataload
+echo "Services started :-)"
